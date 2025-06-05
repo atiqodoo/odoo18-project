0773bb74f9a696a0550411306486d673453d9bf3
atiqodoo <atiqmuritu@gmail.com>
2025-06-05 03:36:23 +0300

    Add custom addons

---
+++ custom_addons/custom_addons/my_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'My Custom Module',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'atiqodoo',
+    'category': 'Custom',
+    'description': 'A custom Odoo 18 module.',
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
